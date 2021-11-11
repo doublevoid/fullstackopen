727faf351f08f79811539998b3631eac1a8448b0
doublevoid <gabrielgarocs@gmail.com>
2021-11-11 18:56:23 -0300

    finishing part 3

--- part2/2.15 ~ 2.17/src/App.js
+++ part2/2.15 ~ 2.17/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [newSearch, setNewSearch] = useState('')
   const [filteredNumbers, setNewFilter] = useState([])
 
-  const baseUrl = 'http://localhost:3001/persons'
+  const baseUrl = '/api/persons'
 
   useEffect(() => {
     console.log('effect')
